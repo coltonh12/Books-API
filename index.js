8062f5a995e242cb713866c2053a2b3917dea910
Colton Hye <hyercolt@msu.edu>
2024-03-14 16:11:34 -0400

    test

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-const supabaseUrl = 'https://vmhdyikknzawqfvslqdl.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtaGR5aWtrbnphd3FmdnNscWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NDMxMzEsImV4cCI6MjAyNjAxOTEzMX0.KY21eyLCqGdjd6U7zMtFWHr5mGS0ogAHld1TMK1rLWE';
-const supabase = createClient(supabaseUrl, supabaseKey);
 
-async function fetchBooks() {
-    let { data: books, error } = await supabase
-        .from('books')
-        .select('*');
+import { createClient } from '@supabase/supabase-js'
 
-    if (error) {
-        console.error('Error fetching books:', error.message);
-        return;
-    }
-
-    let bookList = document.getElementById('Books');
-    bookList.innerHTML = '';
-
-    for (let book of books) {
-        bookList.innerHTML += `<li>${book.title}</li>`;
-    }
-}
-
-fetchBooks();
+const supabaseUrl = 'https://vmhdyikknzawqfvslqdl.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtaGR5aWtrbnphd3FmdnNscWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NDMxMzEsImV4cCI6MjAyNjAxOTEzMX0.KY21eyLCqGdjd6U7zMtFWHr5mGS0ogAHld1TMK1rLWE'
+const supabase = createClient(supabaseUrl, supabaseKey)
