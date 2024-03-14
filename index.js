9f51927d638b91b4d949fc2a60b223e3a0272631
Colton Hye <hyercolt@msu.edu>
2024-03-14 15:44:50 -0400

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add JavaScript below
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://vmhdyikknzawqfvslqdl.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtaGR5aWtrbnphd3FmdnNscWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NDMxMzEsImV4cCI6MjAyNjAxOTEzMX0.KY21eyLCqGdjd6U7zMtFWHr5mGS0ogAHld1TMK1rLWE'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
