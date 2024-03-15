2915c69df4016d3f4b7ddbae5a9a5b85cb904dff
Colton Hye <hyercolt@msu.edu>
2024-03-15 09:05:34 -0400

    k

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// Define Supabase URL and API key
+import { createClient } from '@supabase/supabase-js'
+
 const supabaseUrl = 'https://vmhdyikknzawqfvslqdl.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtaGR5aWtrbnphd3FmdnNscWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NDMxMzEsImV4cCI6MjAyNjAxOTEzMX0.KY21eyLCqGdjd6U7zMtFWHr5mGS0ogAHld1TMK1rLWE';
 
