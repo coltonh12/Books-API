f1b73e84cd7ef57b4bb8f43669c014c0905ca2af
Colton Hye <hyercolt@msu.edu>
2024-03-16 15:18:39 -0400

    k

--- export.mjs
+++ export.mjs
@@ -4,7 +4,6 @@ const supabaseUrl = 'https://vmhdyikknzawqfvslqdl.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtaGR5aWtrbnphd3FmdnNscWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NDMxMzEsImV4cCI6MjAyNjAxOTEzMX0.KY21eyLCqGdjd6U7zMtFWHr5mGS0ogAHld1TMK1rLWE'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-
 let { data: books1, error } = await supabase
   .from('books1')
   .select('*')
